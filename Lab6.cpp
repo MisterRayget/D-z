0ec6c34ab2ed3e7239761f09ffcce0d1c502f28a
MisterRayget <145323958+MisterRayget@users.noreply.github.com>
2023-10-27 17:49:22 +0300

    Add files via upload

---
+++ Lab6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    const int NUM = 10;
+    int arrint[NUM][NUM];
+
+    setlocale(0, "");
+    for (int i = 1; i < NUM; ++i) {
+        for (int j = 1; j < NUM; ++j) {
+            arrint[i][j] = i * j;
+        }
+    }
+    for (int i = 1; i < NUM;++i) {
+        for (int j = 1; j < NUM; ++j) {
+            cout << setw(4) << arrint[i][j];
+        }
+        cout << endl<<endl;
+    }
+
+
+    return 0;
+}
