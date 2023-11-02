7494493d94151a251fad4fbb704f5fdc20286f9c
MisterRayget <145323958+MisterRayget@users.noreply.github.com>
2023-11-02 19:17:10 +0300

    Add files via upload

---
+++ D-z_2.11.23_2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum_of_digits(int n) {
+    int t = 0;
+    while (n != 0) {
+        t += n % 10;
+        n /= 10;
+    }
+    return t;
+}
+
+int main() {
+    setlocale(0, "");
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+
+    int result = n;
+    do {
+        result = sum_of_digits(result);
+        cout << result << endl;
+    } while (result >= 10);
+
+    return 0;
+}
