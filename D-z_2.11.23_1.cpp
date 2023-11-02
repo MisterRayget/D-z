d865286163375ef013a7a0c6423165169800f60f
MisterRayget <145323958+MisterRayget@users.noreply.github.com>
2023-11-02 20:02:15 +0300

    Add files via upload

---
+++ D-z_2.11.23_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+void random();
+const int QWA = 10;
+int main(){
+    setlocale(0, "");
+    srand(time(0));
+    random();
+    return 0;
+}
+void random() {
+    int num1[QWA]{ 1 + rand() % 20,1 + rand() % 20, 1 + rand() % 20, 1 + rand() % 20, 1 + rand() % 20, 1 + rand() % 20, 1 + rand() % 20, 1 + rand() % 20, 1 + rand() % 20, 1 + rand() % 20, };
+    int num2[QWA]{ 1 + rand() % 10,1 + rand() % 10, 1 + rand() % 10, 1 + rand() % 10, 1 + rand() % 10, 1 + rand() % 10, 1 + rand() % 10, 1 + rand() % 10, 1 + rand() % 10, 1 + rand() % 10, };
+    for (int i = 0;i < QWA;i++) {
+        cout << num1[i] << " ";
+    }
+    cout << endl;
+    for (int j = 0;j < QWA;j++) {
+        cout << num2[j] << " ";
+    }
+}
