19ea788f8704ce576d8b012f50b880ae5810c0be
MisterRayget <145323958+MisterRayget@users.noreply.github.com>
2023-11-03 10:40:35 +0300

    Update D-z_2.11.23_1.cpp

--- D-z_2.11.23_1.cpp
+++ D-z_2.11.23_1.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
-#include <cstdlib>
+#include <ctime>
 using namespace std;
 void random();
 const int QWA = 10;
-int main(){
+int main() {
     setlocale(0, "");
     srand(time(0));
     random();
     return 0;
 }
 void random() {
-    int arrnum1[QWA]{} = 0;
-    int arrnum2[QWA]{} = 0;
+    int arrnum1[QWA]{};
+    int arrnum2[QWA]{};
     //Заполнение
-    for (int i;i < QWA;i++) {
-        arrnum1[i] = srand();
+    for (int i=0; i < QWA; i++) {
+        arrnum1[i] = rand();
     }
-    for (int j;j < QWA;j++) {
-        arrnum2[j] = srand();
+    for (int j=0; j < QWA; j++) {
+        arrnum2[j] = rand();
     }
     //Выдача
-    for (int i = 0;i < QWA;i++) {
-        cout << arrint1[i] << " ";
+    for (int i = 0; i < QWA; i++) {
+        cout << arrnum1[i] << " ";
     }
-    for (int j = 0;j < QWA;j++) {
-        cout << arrint2[j] << " ";
+    cout << endl;
+    for (int j = 0; j < QWA; j++) {
+        cout << arrnum2[j] << " ";
     }
 }
