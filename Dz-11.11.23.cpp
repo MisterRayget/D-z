e1df5a8be9bb523b6d2d21a26e355a19e8adc815
MisterRayget <145323958+MisterRayget@users.noreply.github.com>
2023-11-11 22:51:38 +0300

    Add files via upload

---
+++ Dz-11.11.23.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    const int NUM = 4;
+    string arrslov[NUM] = { "импортный","картина","лесной","эскпорт" };
+    for (int i = 0;i < NUM;i++) {
+        for (int j = 0;j < NUM;j++) {
+            cout << arrslov[i]<< " " << arrslov[j]<<"<->" << arrslov[j] << " " << arrslov[i] << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
