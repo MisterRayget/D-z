d681db6ca44898d3053ab82868ff27d8237a32c9
MisterRayget <145323958+MisterRayget@users.noreply.github.com>
2023-11-14 10:09:45 +0300

    Update Dz-11.11.23.cpp

--- Dz-11.11.23.cpp
+++ Dz-11.11.23.cpp
@@ -1,11 +1,17 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    setlocale(0, "");
+    setlocale(0, " ");
     const int NUM = 4;
-    string arrslov[NUM] = { "импортный","картина","лесной","эскпорт" };
+    string arrslov[NUM];
+    cout<<"Введите слова"<<endl;
+    for(int i = 0;i < NUM;i++){
+        gitline(cin, arrslov[i]);
+    }
+    cout<<endl;
     for (int i = 0;i < NUM;i++) {
         for (int j = 0;j < NUM;j++) {
             cout << arrslov[i]<< " " << arrslov[j]<<"<->" << arrslov[j] << " " << arrslov[i] << endl;
