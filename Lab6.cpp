ac7629347b5f2408a7a84dbfea866ce40cbe631e
MisterRayget <145323958+MisterRayget@users.noreply.github.com>
2023-11-24 08:45:10 +0300

    Update Lab6.cpp

--- Lab6.cpp
+++ Lab6.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main(){
-    const int NUM = 10;
+    const int NUM = 9;
     int arrint[NUM][NUM];
 
     setlocale(0, "");
-    for (int i = 1; i < NUM; ++i) {
-        for (int j = 1; j < NUM; ++j) {
-            arrint[i][j] = i * j;
+    for (int i = 0; i < NUM; ++i) {
+        for (int j = 0; j < NUM; ++j) {
+            arrint[i][j] = (1+i) * (1+j);
         }
     }
-    for (int i = 1; i < NUM;++i) {
-        for (int j = 1; j < NUM; ++j) {
+    for (int i = 0; i < NUM;++i) {
+        for (int j = 0; j < NUM; ++j) {
             cout << setw(4) << arrint[i][j];
         }
         cout << endl<<endl;
