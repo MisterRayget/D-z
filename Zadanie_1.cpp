0b8c0ff72e70e9fa29c595fab9b5cf09c0453c52
MisterRayget <145323958+MisterRayget@users.noreply.github.com>
2023-12-15 10:27:22 +0300

    Add files via upload

---
+++ Zadanie_1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+class captan {
+private:
+    int arr[4][2];
+    int arr2[2][4];
+public:
+    void ZAPMASSIV() {
+        int num = 0;
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < 2; i++) {
+                num += 11;
+                arr[j][i] = num;
+                arr2[i][j] = num;
+            }
+        }
+    }
+    void OTPMASSIV() {
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 4; i++) {
+                cout << arr2[j][i] << " ";
+            }
+            cout << endl;
+        }
+    }
+    void OTP2MASSIV() {
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < 2; i++) {
+                cout << arr[j][i] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main(){
+    setlocale(0, "");
+    captan objMASS;
+    objMASS.ZAPMASSIV();
+    objMASS.OTP2MASSIV();
+    cout << endl;
+    objMASS.OTPMASSIV();
+    return 0;
+}
